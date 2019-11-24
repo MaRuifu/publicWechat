9c37bed4a68b2187e3d05fe71a8a2effaa1aa418
unknown <mrf_it@163.com>
2019-11-24 23:25:43 +0800

    maruifu

--- src/main/java/com/maruifu/wechat/controller/UserController.java
+++ src/main/java/com/maruifu/wechat/controller/UserController.java
@@ -2,8 +2,11 @@ package com.maruifu.wechat.controller;
 
 import com.maruifu.wechat.pojo.dmo.BaseUser;
 import com.maruifu.wechat.service.BaseUserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,13 +23,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+
     @Autowired
     @Qualifier("baseUserServiceImpl")
     private BaseUserService baseUserService;
 
     @RequestMapping("/user/queryById")
     @ResponseBody
-    public BaseUser show(Integer id) {
+    public BaseUser show(@RequestBody  Integer id) {
+        logger.info("查询用户1");
+        logger.error("查询用户2");
+
         return baseUserService.selectByPrimaryKey(id);
     }
 
