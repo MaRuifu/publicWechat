7b01172d84af3277b77a5bb2da12fbf5a143de7b
xiaomage <maruifu@bjmantis.com>
2019-11-27 13:27:57 +0800

    maruifu

--- src/main/java/com/maruifu/wechat/WechatApplication.java
+++ src/main/java/com/maruifu/wechat/WechatApplication.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackages = "com.maruifu.wechat") //1
-@MapperScan(basePackages = "com.maruifu.wechat")
+@MapperScan(basePackages = "com.maruifu.wechat.dao")
 public class WechatApplication {
 
 	public static void main(String[] args) {
